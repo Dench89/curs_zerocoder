c26016d06adabe9c1d907c903d71a0c1261fbe41
Abon89 <denis.yamal@gmail.com>
2026-06-29 00:31:57 +0500

    first commit

--- main.py
+++ main.py
@@ -1,14 +1,1 @@
-import requests
-
-url = "https://jsonplaceholder.typicode.com/posts/1"
-
-response = requests.get(url)
-
-print(f"Статус-код: {response.status_code}")
-
-data = response.json()
-
-print(f"ID: {data['id']}")
-print(f"Заголовок: {data['title']}")
-print(f"Первые 100 символов текста:")
-print(data['body'][:100])
+print("Hello world")
